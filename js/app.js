f4615a54782b97e6a0bba7d44bb4f4ec8df08a10
object-ions <moshikoatia@gmail.com>
2023-06-09 22:17:55 -0700

    try a different way to solve the tests

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let input;
 let arrNumbers = [];
 let arrDigits = [];
-
+let arrRunningNumbers = [];
 function beepBoop(input) {
 
   for (let i = 0; i <= input; i++) {
@@ -21,31 +21,35 @@ function beepBoop(input) {
 
 let outputString = "";
 
-// arrNumbers.forEach(function numbersToDigits(number) {
-//   console.log("the number: " + number);
-//   arrDigits = number.split("");
-//   console.log("the number after split: " + arrDigits);
+function try () {
+  arrRunningNumbers.forEach(function numbersToDigits(number) {
+    console.log("the number: " + number);
+    arrDigits = arrDigits.toString();
+    arrDigits = number.split("");
+    console.log("the number after split: " + arrDigits);
 
-//   outputString = outputString + beepBoop(input) + "  ";
-// });
 
-let arrRunningNumbers = [];
+  });
+}
+
+
 
 
 function runningNumbers(input) {
   for (let i = 0; i <= input; i++) {
     arrRunningNumbers[i] = i;
   }
+
   console.log(arrRunningNumbers);
 }
 
 
 function numbersToDigits(number) {
+
   console.log("the number: " + number);
-  arrDigits = number.split("");
+  arrDigits = number.split(",");
   console.log("the number after split: " + arrDigits);
 
-  outputString = outputString + beepBoop(input) + "  ";
 }
 
 function replaceIncidents(arrNumbers) {
