156b1f3eed52d4d479db9a9a757664b89c875166
object-ions <moshikoatia@gmail.com>
2023-06-10 12:57:31 -0700

    add a condition: if the user dont set value to the input it will show error

--- js/app.js
+++ js/app.js
@@ -15,21 +15,26 @@ form.addEventListener('submit', function getInput(event) {
 
 // Buisness Logic:
 function beepBoop(input) {
-  arrDigits = [];
-  for (let i = 0; i <= input; i++) {
-    arrDigits[i] = i.toString();
 
-    if (arrDigits[i].includes('3')) {
-      arrDigits[i] = '\"Won\'t you be my neighbor?\"';
-    } else if (arrDigits[i].includes('2')) {
-      arrDigits[i] = '\"Boop!\"';
-    } else if (arrDigits[i].includes('1')) {
-      arrDigits[i] = '\"Beep\"';
+  if (input === null) {
+    console.log("error");
+  } else {
+    arrDigits = [];
+    for (let i = 0; i <= input; i++) {
+      arrDigits[i] = i.toString();
+
+      if (arrDigits[i].includes('3')) {
+        arrDigits[i] = '\"Won\'t you be my neighbor?\"';
+      } else if (arrDigits[i].includes('2')) {
+        arrDigits[i] = '\"Boop!\"';
+      } else if (arrDigits[i].includes('1')) {
+        arrDigits[i] = '\"Beep\"';
+      };
     };
-  };
 
-  output.innerText = arrDigits.join(', ');
-  displayDiv.classList.remove('hidden');
-  document.getElementById('input').value = null;
+    output.innerText = arrDigits.join(', ');
+    displayDiv.classList.remove('hidden');
+    document.getElementById('input').value = null;
+  }
 
 };
