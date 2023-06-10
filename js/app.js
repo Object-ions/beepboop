4a6374fa501ce11e8b6d92cd94aed4e351457f71
object-ions <moshikoatia@gmail.com>
2023-06-10 11:29:47 -0700

    add event listener to the app, and refacor the add as well as the html file

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-let form = document.getElementById('form').value;
-let input = document.getElementById('input');
-let output = document.getElementById('output');
 
+let input;
 let arrDigits = [];
-function beepBoop(input) {
+let displayDiv = document.getElementById('display-div');
+let output = document.getElementById('output');
+let form = document.getElementById('form');
 
-  // working
+function beepBoop(input) {
   for (let i = 0; i <= input; i++) {
     arrDigits[i] = i.toString();
 
@@ -18,11 +18,15 @@ function beepBoop(input) {
     };
   };
   console.log(arrDigits);
+  output.innerText = arrDigits;
+  displayDiv.removeAttribute('class')
 
-}
+};
 
 form.addEventListener('submit', function getInput(event) {
   event.preventDefault();
+  input = document.getElementById('input').value
   console.log(input);
-})
+  beepBoop(input);
+});
 
