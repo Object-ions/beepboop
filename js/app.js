4a057f6d2da308c10bf79e93a681db874e4532c0
object-ions <moshikoatia@gmail.com>
2023-06-09 17:45:08 -0700

    add a function to the app (numberaToDigits)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let input;
 let arrNumbers = [];
-let digits = [];
+let arrDigits = [];
 
 function beepBoop(input) {
   for (let i = 0; i <= input; i++) {
@@ -19,5 +19,6 @@ function beepBoop(input) {
 }
 
 function numbersToDigits(arrNumbers) {
-
+  arrDigits = arrNumbers.split('');
+  console.log(arrDigits);
 }
